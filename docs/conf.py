a06c8a909b7328464b9389453e40c08ad6eb9884
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-08-07 10:37:09 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -203,7 +203,7 @@ latex_elements = {
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
