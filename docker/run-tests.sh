a03ed6369a52e031a41b2140050a6ffb42b16eb0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2017-05-05 17:47:36 +0200

    Fixed

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -4,6 +4,7 @@ set -e
 apt-get update --fix-missing && apt-get install -y openjdk-7-jre-headless 
 cd /root/tcoffee/testsuite/
 java -jar black-coffee.jar \
+  --var tcoffee.home=/opt/tcoffee \
   --stop error \
   --delete never \
   --sandbox-dir /test-results/all \
