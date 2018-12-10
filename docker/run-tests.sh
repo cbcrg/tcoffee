5a93282a2cc8eb21c28a91483aca97b68a8e6a8a
Cedric Notredame <cedric.notredame@gmail.com>
2018-12-10 18:21:46 +0100

    other attempt to remove all the tests

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -3,12 +3,11 @@ set -e
 
 apt-get update --fix-missing && apt-get install -y openjdk-7-jre-headless 
 cd /root/tcoffee/testsuite/
-touch /test-results/index.html
-#java -jar black-coffee.jar \
-#  --var tcoffee.home=/opt/tcoffee \
-#  --stop error \
-#  --delete never \
-#  --sandbox-dir /test-results/all \
-#  --html-path-prefix all \
-#  -o /test-results/index.html \
-#  -R ./all
+java -jar black-coffee.jar \
+  --var tcoffee.home=/opt/tcoffee \
+  --stop error \
+  --delete never \
+  --sandbox-dir /test-results/all \
+  --html-path-prefix all \
+  -o /test-results/index.html \
+  -R ./all
