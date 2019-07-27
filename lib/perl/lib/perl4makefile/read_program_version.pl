9ad2f069f268ceb767a5d0727a98393e0973a810
Cedric Notredame <cedric.notredame@gmail.com>
2019-07-27 17:28:02 +0200

    test

--- lib/perl/lib/perl4makefile/read_program_version.pl
+++ lib/perl/lib/perl4makefile/read_program_version.pl
@@ -1,63 +1,72 @@
 #!/usr/bin/env perl
 use strict;
 
-#This program will set the version tag to whatever is in $ARGV[0] or the file ./version_number-version
-#if on MASTER system, it will set this value to the Github branch value
+#This program will set versio_number.version
+#if on MASTER system, the github branch numb er will be updated
 #it will return stdout
 
 my $file;
-
-my $major_versionF;
+my $build_versionF;
 my $minor_versionF;
+my $major_versionF;
 my $githubF;
 my $version_numberF;
 
-my $major_version;
+my $build_version;
 my $minor_version;
+my $major_version;
 my $github;
 my $version_number;
 
-my $beta
-my $stable;
-my $major;
-
 my $path;
-
 my $cl=join( " ", @ARGV);
-if ( ($cl=~/-path=\s*(\S+)/))
+
+if ( ($cl=~/-path=\s*(\S+)/)){$path=$1;}
+else {$path="./";}
+
+$build_versionF=$path."/build_version.version";
+$minor_versionF=$path."/minor_version.version";
+$major_versionF=$path."/major_version.version";
+$githubF=$path."/github.version";
+$version_number=$path."/version_number.version";
+
+
+if (!-e $build_versionF){print STDERR "build_version File [$build_versionF] could not be opened [FATAL:read_program_version.pl]\n";die;}
+if (!-e $minor_versionF){print STDERR "minor_version File [$minor_versionF] could not be opened [FATAL:read_program_version.pl]\n";die;}
+if (!-e $major_versionF){print STDERR "major_version File [$major_versionF] could not be opened [FATAL:read_program_version.pl]\n";die;}
+
+if (($cl=~/-beta/))
+  {increase ($build_versionF, 1);}
+elsif ( ($cl=~/-stable/))
   {
-    $path=$1;
-    $major_versionF=$path."/major_version.version";
-    $minor_versionF=$path."/minor_version.version";
-    $githubF=$path."/github.version";
-    $version_number=$path."/version_number.version";
+    increase   ($minor_versionF, 1);
+    value2file(0, $build_versionF);
   }
-if (!-e $major_versionF)return 1;
-if (!-e $minor_versionF)return 1;
-
-if (($cl=~/-beta/)){increase ($minor_versionF, 1);}
-elsif ( ($cl=~/-stable/)){increase ($minor_versionF, 1);}
-elsif ( ($cl=~/-major/)) {increase ($major_versionF, 1);}
-
+elsif ( ($cl=~/-major/))
+  {
+    increase   ($major_versionF, 1);
+    value2file(0, $build_versionF);
+    value2file(0, $minor_versionF);
+  }
 
 if ($ENV{'TC_MASTER_NODE'}){system ("git branch -v  >$githubF");}
 
 
-
+$build_version=file2value($build_versionF);
 $minor_version=file2value($minor_versionF);
 $major_version=file2value($major_versionF);
 $github=github_file2value($githubF);
 
-$version_number="$major_version\.$minor_version\.$github";
+$version_number="$major_version\.$minor_version\.$build_version\.$github";
 value2file($version_number, $version_numberF);
-print "$version_number";
+print "$version_number\n";
 
 
 
 sub file2value
   {
     my ($file)=@_;
-    open (F, "$file");
+    open (F, "$file") or die "Could not open file [$file][FATAL:read_program_version.pl\n";
     while (<F>)
       {
 	my $l=$_;
@@ -66,12 +75,12 @@ sub file2value
 	return $l;
       }
   }
-github_file2value
+sub github_file2value
     {
       my ($file)=@_;
       my $value;
       
-      open (F, "$file");
+      open (F, "$file") or die "Could not open file [$file][FATAL:read_program_version.pl\n";
       while (<F>)
 	{
 	  my $l=$_;
@@ -80,17 +89,16 @@ github_file2value
 	    {
 	      $value=$1;
 	    }
-	  
-	  
 	  close (F);
-	return $l;
-      }
-  }
+	  return $value;
+	}
+    }
+
 sub value2file
     {
       my ($value,$file)=@_;
       open (F, ">$file");
-      print F "$vlaue\n";
+      print F "$value\n";
       close F;
     }
     
@@ -98,9 +106,9 @@ sub value2file
 sub increase
     {
       my ($file, $inc)=@_;
-      my $value=$file2value($file);
+      my $value=file2value($file);
       $value+=$inc;
-      $value2file($value, $file);
+      value2file($value, $file);
       return $value;
     }
 
@@ -111,57 +119,3 @@ sub increase
 
 
 
-branchF;
-
-my $version;
-my $branch;
-
-
-
-if( $ARGV[0]){$file=$ARGV[0];}
-else{$file="version_number";}
-
-$versionF=$file.".version";
-$branchF=$file.".branch";
-
-
-if ($ENV{'TC_MASTER_NODE'})
-  {
-    system ("git branch -v  > tmp.txt");
-    open (F, "tmp.txt");
-    while(<F>)
-      {
-	my $l=$_;
-	if ($l=~/\* master (\w+) .*/)
-	  {
-	    $branch=$1;
-	  }
-      }
-    close (F);
-    open (F, ">$branchF");
-    print F "$branch";
-    close F;
-    unlink ("tmp.txt");
-  }
-
-
-#get version
-open (F,$versionF);
-while (<F>)
-          {
-	  $version=$_;
-	  chop ($version);
-	}
-close (F);
-
-
-
-#get branch
-open (F,$branchF);
-while (<F>)
-          {
-	  $branch=$_;
-	  chop ($branch);
-	}
-close (F);
-print "$version.$branch";
