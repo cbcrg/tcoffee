de39e8392c19df2bffb7e2c9f3b4d1a7a35906ca
Cedric Notredame <cedric.notredame@gmail.com>
2019-07-28 16:37:38 +0200

    test path 9

--- build/build.sh
+++ build/build.sh
@@ -326,7 +326,7 @@ function build_binaries()
 	cp $WORKSPACE/tcoffee/build/extra/secondary_struc.py $TCDIR/plugins/$OSNAME/secondary_struc.py
 	chmod +x $TCDIR/plugins/$OSNAME/secondary_struc.py
 	
-	# HH_seach
+	# HH_search
 	cp $WORKSPACE/tcoffee/build/extra/HHsearch_1.5.1/$OSNAME-$OSARCH/* $TCDIR/plugins/$OSNAME/	
 }
 
