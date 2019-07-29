f93850452ec4b67415c99f52859285682fca5e43
Cedric Notredame <cedric.notredame@gmail.com>
2019-07-29 21:13:30 +0200

    test circle CI for stable 4

--- lib/perl/lib/scripts/install.pl
+++ lib/perl/lib/scripts/install.pl
@@ -12,7 +12,7 @@ our (%MODE, %PG, %ENV_SET, %SUPPORTED_OS);
 #DO NOT EDIT MANUALLY START
 #To be populated with edit_version.pl 
 our $VERSION="0";
-#__#VERSION
+XXXXVERSION
 #DO NOT EDIT MANUALLY END
 our $EXIT_SUCCESS=0;
 our $EXIT_FAILURE=1;
