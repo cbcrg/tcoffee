749235e4388e6528a7d40bc0bdec5a3a4242addd
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-03 13:34:52 +0200

    changed the tests

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.30.7.b7b28e8',
+    VERSION: 'Version_13.30.8.d8290f9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
