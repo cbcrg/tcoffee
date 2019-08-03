8083152b6b58ba299692de9715983afa44c2776c
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-03 13:48:28 +0200

    changed the tests 2

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -3,4 +3,4 @@ set -e
 
 #apt-get update --fix-missing && apt-get install -y openjdk-7-jre-headless 
 cd /root/tcoffee/
-lib/perl/lib/doc2test.pl -replay ./tests/.dumps/core.tests/
+/root/tcoffee/lib/perl/lib/doc2test.pl -replay ./tests/.dumps/core.tests/
