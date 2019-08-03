1c6be54e1ec82164326cae214ab67a8afa4a9235
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-03 23:22:43 +0200

    new stable distribution 6

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.31.8.0d91576',
+    VERSION: 'Version_13.31.9.84e1c87',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
