d7f6d402e3c4c84c462769f5934422a635680266
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-03 20:33:19 +0200

    fixed many exemples and the doc2test.pl 2

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.30.13.6303e1b',
+    VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
