ea5aacc47c9bedb6bd1b6821ab2aba7d04650434
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-05 16:11:12 +0200

    simplified versionning

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.32.28.500746e',
+    VERSION: 'Version_13.32.29.d782e24',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
