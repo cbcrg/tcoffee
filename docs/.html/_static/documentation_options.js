46b2c3f067219f98e227a0dd5251e6c800d483a1
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-05 20:12:56 +0200

    fixed confi.yml typo

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.38.2.df65fac',
+    VERSION: 'Version_13.38.3.a77ca2b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
