d60cb65f8a88e3dd7a0cf00cb3b57e4924e2291f
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-05 18:16:23 +0200

    fixed error config.yum

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.35.0.03e022f',
+    VERSION: 'Version_13.36.0.2f2f8f5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
