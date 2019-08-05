eb855b1b190d159c8f4d37dd81246bf30840787e
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-05 14:24:26 +0200

    cleaned the publishing procedure in ./circleci/config.yml

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.32.10.62a0b55',
+    VERSION: 'Version_13.32.11.743c221',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
