a96e56523ee6c2457a7bd1e464380f7bc8a596fa
Cedric Notredame <cedric.notredame@gmail.com>
2019-08-08 13:41:32 +0200

    added all the tests dumps to git

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.40.3.86a4107',
+    VERSION: 'Version_13.40.4.c6fb5cf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
