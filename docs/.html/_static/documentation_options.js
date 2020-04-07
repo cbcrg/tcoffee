d50f16526cd16a9f41b5be9b63b1c66e9511c466
Cedric Notredame <cedric.notredame@gmail.com>
2020-04-07 23:38:49 +0200

    undocumented beta release tested with beta tests

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.41.19.b749c6d',
+    VERSION: 'Version_13.41.20.ff39c04',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
