a647a9b53dfb44bba9c0f98dd2dd562f5f5dcaa8
JoseEspinosa <kadomu@gmail.com>
2023-05-26 22:46:54 +0200

    undocumented beta release tested with beta tests

--- docs/.html/_static/documentation_options.js
+++ docs/.html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version_13.45.69.3c310a9',
+    VERSION: 'Version_13.45.70.3c310a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'singlehtml',
