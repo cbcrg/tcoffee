f9334b7ad660fefbbb29535aeb4740049a1cd0bc
JoseEspinosa <kadomu@gmail.com>
2026-07-21 22:53:12 +0200

    Change commandline_tools_os_x s3 url

--- build/install-osx-cmdlinetools.sh
+++ build/install-osx-cmdlinetools.sh
@@ -5,7 +5,7 @@ set -e
 # See http://apple.stackexchange.com/a/107314
 # 
 sudo rm -rf /Applications/Xcode.app/
-wget -q --no-check-certificate https://s3-eu-west-1.amazonaws.com/cbcrg-eu/tcoffee-ci/commandline_tools_os_x_mountain_lion_for_xcode__march_2014.dmg
+wget -q --no-check-certificate https://s3-eu-west-1.amazonaws.com/proteinfold-dataset/tcoffee/tcoffee-ci/commandline_tools_os_x_mountain_lion_for_xcode__march_2014.dmg
 hdiutil attach commandline_tools_os_x_mountain_lion_for_xcode__march_2014.dmg
 cd "/Volumes/Command Line Tools (Mountain Lion)"/
 sudo installer -verbose -pkg "Command Line Tools (Mountain Lion).mpkg" -target /
